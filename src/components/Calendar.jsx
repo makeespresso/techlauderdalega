a882bec606ab97bf96c070383b6dc6163209dd50
Ana S Benedict <anasbenedict@Anas-MBP.lan>
2020-05-03 15:07:08 -0500

    Calendar set up

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -3,8 +3,50 @@ import React from 'react';
 const Header = () => {
   return (
  <div className="Calendar">
+   <div className="Information">
+      <div className="Date">May 1</div>
+        <div className="Text">text</div>
+        <div className="Text">text</div>
+        <div className="Text">text</div>
+        <div className="Text">text</div>
+        <div className="Text">text</div>
+      </div>
 
- </div>
+      <div className="Information">
+      <div className="Date">May 1</div>
+        <div className="Text">text</div>
+        <div className="Text">text</div>
+        <div className="Text">text</div>
+        <div className="Text">text</div>
+        <div className="Text">text</div>
+      </div>
+
+      <div className="Information">
+      <div className="Date">May 1</div>
+        <div className="Text">text</div>
+        <div className="Text">text</div>
+        <div className="Text">text</div>
+        <div className="Text">text</div>
+        <div className="Text">text</div>
+      </div>
+
+      <div className="Information">
+      <div className="Date">May 1</div>
+        <div className="Text">text</div>
+        <div className="Text">text</div>
+        <div className="Text">text</div>
+        <div className="Text">text</div>
+        <div className="Text">text</div>
+      </div>
+<div className="Information">
+      <div className="Date">May 1</div>
+        <div className="Text">text</div>
+        <div className="Text">text</div>
+        <div className="Text">text</div>
+        <div className="Text">text</div>
+        <div className="Text">text</div>
+      </div>
+    </div>
   )
 }
 
