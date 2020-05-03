006ec6a0c9474e962a64a3ded70a8e728b88bcdd
Ana S Benedict <anasbenedict@Anas-MBP.lan>
2020-05-02 21:08:47 -0500

    Init

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<div className="Teal">
+  <h4>We believe that learning has no limits.</h4>
+<p>Whether you're looking for a new job or wanting to diversify your skill set to become more employable, our community is here for you, online.</p>
+<br></br>
+<p>That's why we're excited to announce a partnership with General Assembly- the gloabl leader in skills-based education.</p>
+<br></br>
+<p><b>Every Friday in May, join workshops in coding, data, marketing, UX/UI desing and career development - for free!</b></p>
+</div>
+
+<div className="ClassContainer">
+<div className="InfoBoxes">
+  <div className="Date">
+  <p>May 1</p>
+  </div>
+  <div className="Information">
+<ul>
+  <li>Intro to Data Analytics.</li>
+  <li>Customer Journey Mapping</li>
+  <li>Project Management Workshop</li>
+  <li>How to Create a Supporting Mindset While Job Seeking</li>
+</ul>
+  </div>
+</div>
+
+<div className="InfoBoxes">
+  <div className="Date">
+  <p>May 8</p>
+  </div>
+  <div className="Information">
+<ul>
+  <li>Intro to google Analytics</li>
+  <li>Data Visualization & Intro to Tableau</li>
+  <li>Intro to UX Design</li>
+  <li>Balancing Strategic Job Seeking With Life During COVID</li>
+</ul>
+  </div>
+</div>
+
+<div className="InfoBoxes">
+  <div className="Date">
+  <p>May 15</p>
+  </div>
+  <div className="Information">
+<ul>
+  <li>SEO Training for Begginers</li>
+  <li>User Research Methods</li>
+  <li>Strengthen Your Community Through Thought Leadership</li>
+</ul>
+  </div>
+</div>
+
+<div className="InfoBoxes">
+  <div className="Date">
+  <p>May 22</p>
+  </div>
+  <div className="Information">
+<ul>
+  <li>Excel for Begginers</li>
+  <li>Intro to Coding: HTML & CSS</li>
+  <li>Intro to agile & Scrum</li>
+  <li>Job Search Smarter With Boolean Logic</li>
+</ul>
+  </div>
+</div>
+
+<div className="InfoBoxes">
+  <div className="Date">
+  <p>May 29</p>
+  </div>
+  <div className="Information">
+<ul>
+  <li>Project Management Workshop</li>
+  <li>Customer Journey Mapping</li>
+  <li>Intro to Data Analytics</li>
+  <li>Digital Interviews: Presenting Your Best Self Online</li>
+</ul>
+  </div>
+</div>
+
+
+</div>
+
+</div>
+  );
+}
+
+export default App;
