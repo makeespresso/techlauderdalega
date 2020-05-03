a6dcc60ea009ea2b2126d5fc4f3be66c3999d138
Ana S Benedict <anasbenedict@Anas-MBP.lan>
2020-05-03 11:14:18 -0500

    Banner responsive

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Banner = () => {
   return (
 <div className="Banner center">
-    <div className="InfoHeader">
+    <div className="HeaderText">
     <p>That's why we are excited to announce a partnership with <spam><b>General Assembly</b></spam> - The global leader in skills-based education! </p>
     <p>Every friday in May, join workshops in coding, data marketing, UX design and career development - <b>for free!</b></p>
 
