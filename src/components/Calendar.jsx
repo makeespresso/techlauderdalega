d4642a2fe66b65fe073089cb2b84ea2681901822
Ana S Benedict <anasbenedict@Anas-MBP.lan>
2020-05-03 16:14:05 -0500

    Calendar information up to date

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -4,47 +4,52 @@ const Header = () => {
   return (
  <div className="Calendar">
    <div className="Information">
-      <div className="Date">May 1</div>
-        <div className="Text">text</div>
-        <div className="Text">text</div>
-        <div className="Text">text</div>
-        <div className="Text">text</div>
-        <div className="Text">text</div>
+   <div className="Date">
+        <h2>May 1 </h2>
+      </div>
+        <div className="Text"><p>Intro to Data Analytics</p></div>
+        <div className="Text"><p>Customer Journey Mapping</p></div>
+        <div className="Text"><p>Project Management Workshop</p></div>
+        <div className="Text"><p>How to Create a Supporting Mindset WHile Job Seeking</p></div>
       </div>
 
       <div className="Information">
-      <div className="Date">May 1</div>
-        <div className="Text">text</div>
-        <div className="Text">text</div>
-        <div className="Text">text</div>
-        <div className="Text">text</div>
-        <div className="Text">text</div>
+      <div className="Date">
+        <h2>May 8 </h2>
+      </div>
+        <div className="Text"><p>Intro to google Analytics</p></div>
+        <div className="Text"><p>Data Visualization & Intro to Tableau</p></div>
+        <div className="Text"><p>Intro to UX Design</p></div>
+        <div className="Text"><p>Balancing Strategic Job Seeking With Life During COVID</p></div>
       </div>
 
       <div className="Information">
-      <div className="Date">May 1</div>
-        <div className="Text">text</div>
-        <div className="Text">text</div>
-        <div className="Text">text</div>
-        <div className="Text">text</div>
-        <div className="Text">text</div>
+      <div className="Date">
+        <h2>May 15 </h2>
+      </div>
+        <div className="Text"><p>SEO Training for Begginers</p></div>
+        <div className="Text"><p>User Research Methods</p></div>
+        <div className="Text"><p>Strengthen Your Community Through Leadership</p></div>
       </div>
 
       <div className="Information">
-      <div className="Date">May 1</div>
-        <div className="Text">text</div>
-        <div className="Text">text</div>
-        <div className="Text">text</div>
-        <div className="Text">text</div>
-        <div className="Text">text</div>
+      <div className="Date">
+        <h2>May 22 </h2>
       </div>
+        <div className="Text"><p>Excel for Begginers</p></div>
+        <div className="Text"><p>Intro to Coding: HTML & CSS</p></div>
+        <div className="Text"><p>Intro to Agile & Scrum</p></div>
+        <div className="Text"><p>Job Search Smarter With Boolean Logic</p></div>
+      </div>
+
 <div className="Information">
-      <div className="Date">May 1</div>
-        <div className="Text">text</div>
-        <div className="Text">text</div>
-        <div className="Text">text</div>
-        <div className="Text">text</div>
-        <div className="Text">text</div>
+<div className="Date">
+        <h2>May 29 </h2>
+      </div>
+        <div className="Text"><p>Project Management Workshop</p></div>
+        <div className="Text"><p>Customer Journey Mapping</p></div>
+        <div className="Text"><p>Intro to Data Analytics</p></div>
+        <div className="Text"><p>Digital Interviews: Presenting Your Best Self Online</p></div>
       </div>
     </div>
   )
