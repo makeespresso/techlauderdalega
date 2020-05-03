702cf36bffbe2a6eda3635fb261d4e7db36c1830
Ana S Benedict <anasbenedict@Anas-MBP.lan>
2020-05-03 11:08:28 -0500

    banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,9 +2,30 @@ import React from 'react';
 
 const Banner = () => {
   return (
-<div className="Banner">
-    
+<div className="Banner center">
+    <div className="InfoHeader">
+    <p>That's why we are excited to announce a partnership with <spam><b>General Assembly</b></spam> - The global leader in skills-based education! </p>
+    <p>Every friday in May, join workshops in coding, data marketing, UX design and career development - <b>for free!</b></p>
+
+    </div>
+    {/* <svg
+      height={100}
+      width={100}
+      viewBox="0 0 670 670"
+      fillRule="evenodd"
+      clipRule="evenodd"
+      shapeRendering="geometricPrecision"
+      textRendering="geometricPrecision"
+      imageRendering="optimizeQuality"
+    >
+      <path d="M335 670c185 0 335-150 335-335S520 0 335 0 0 150 0 335s150 335 335 335zm0-274L180 213l-29 26 184 218 185-218-30-26-155 183z" />
+    </svg> */}
+   
+
+
+
 </div>
+
   )
 }
 
