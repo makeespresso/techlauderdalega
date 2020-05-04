ab9e67fdf9491b9a141679ab03c82a15ffb65173
Ana S Benedict <anasbenedict@Anas-MBP.lan>
2020-05-03 21:51:40 -0500

    pre-final version_

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Calendar from './components/Calendar';
 import BannerImg from './components/BannerImg';
 import BannerEvents from './components/ BannerEvents';
 
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="General">
@@ -22,6 +24,7 @@ function App() {
      <br></br>
      <br></br>
      <Calendar />
+     <Footer />
      </div>
      </div>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+<div className="Bannerfooter center">
+<small>©All Rights Reserved, 2020</small>
+</div>
+
+  )
+}
+
+export default Footer;
