2f46473065b296ccb9de9237b8a39e190672132b
Ana S Benedict <anasbenedict@Anas-MBP.lan>
2020-05-03 23:21:18 -0500

    Fixed Public Content, GA logo

---
+++ build/precache-manifest.b630462770a518fc3c7f0ca3afa240ae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b4351f26c7d8c32f28b66a9d571fa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a945acbef019950fa75",
+    "url": "/static/css/main.7f154834.chunk.css"
+  },
+  {
+    "revision": "b9a653a7ae64dea0cde5",
+    "url": "/static/js/2.f0d198fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f0d198fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a945acbef019950fa75",
+    "url": "/static/js/main.5c9119a9.chunk.js"
+  },
+  {
+    "revision": "3a77d49726cd5b9eb4c5",
+    "url": "/static/js/runtime-main.1090ca68.js"
+  },
+  {
+    "revision": "b9404b2d8b4ee0efee31d9ecbba8f5e4",
+    "url": "/static/media/coverimage.b9404b2d.png"
+  },
+  {
+    "revision": "20d8ed95948846c3dc232ded0fc76d90",
+    "url": "/static/media/headers.20d8ed95.png"
+  },
+  {
+    "revision": "0ae70a2d5b116124eb14952f6e5be949",
+    "url": "/static/media/heroimage.0ae70a2d.png"
+  }
+]);
