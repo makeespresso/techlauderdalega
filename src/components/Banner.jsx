1ca06c76b30e2a3af8e146aab33d097587c26647
Ana S Benedict <anasbenedict@Anas-MBP.lan>
2020-05-03 21:55:59 -0500

    pre-final version fixed color on events

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
 <div className="Banner center">
     <div className="HeaderText">
     <p>That's why we are excited to announce a partnership with <spam><b>General Assembly</b></spam> - The global leader in skills-based education! </p>
-    <p>Every friday in May, join workshops in coding, data marketing, UX design and career development - <b style={{color: "#FF2834"}}>for free!</b></p>
+    <p>Every friday in May, join workshops in coding, data marketing, UX design and career development - <b style={{color: "#F2486D"}}>for free!</b></p>
 </div>
 </div>
 
